ddb57996b427e0dc8909a6296cc130dfed69cc67
IsraelBanez <isbanez@calpoly.edu>
2024-01-22 16:41:48 -0800

    no major change except transitioning into cookies

--- chitsystem/src/main/java/com/chit/chitsystem/service/AuthenticationService.java
+++ chitsystem/src/main/java/com/chit/chitsystem/service/AuthenticationService.java
@@ -21,6 +21,9 @@ import com.chit.chitsystem.exception.newexceptions.UserNotFoundException;
 import com.chit.chitsystem.repository.TokenRepository;
 import com.chit.chitsystem.repository.UserRepository;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -66,19 +69,6 @@ public class AuthenticationService {
         }
     }
 
-    // Helper method to build and store the Token entity
-    private void storeUserToken(User user, String jwtToken, String refreshToken) {
-        var token = Token.builder()
-            .user(user)
-            .accessToken(jwtToken)
-            .refreshToken(refreshToken)
-            .tokenType(TokenType.BEARER)
-            .expired(false)
-            .revoked(false)
-            .build();
-        tokenRepository.save(token);
-    }
-
     // Sign in and validate users
     public JWTAuthenticationResponse signInUser(SignInRequest request) {
         try {
@@ -91,12 +81,35 @@ public class AuthenticationService {
             var jwt = jwtService.generateToken(user);
             var refreshToken = jwtService.generateRefreshToken(user);
             revokeAllUserTokens(user); // Revoke all user tokens
-            storeUserToken(user, jwt, refreshToken); // StoreeshToken); // Store 
+            storeUserToken(user, jwt, refreshToken); // Store Token
             return JWTAuthenticationResponse.builder().accessToken(jwt).refreshToken(refreshToken).build();
         } catch (Exception e) {
             log.error("[AuthenticationService] - Error during sign in.", e);
             throw e;
         } 
+    }
+
+    // Helper method to build http-only cookies
+    private void createHTTPOnlyCookie(String name, String value, int maxAge, HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(maxAge);
+        cookie.setSecure(request.isSecure()); // Set to true if using HTTPS
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
+    // Helper method to build and store the Token entity
+    private void storeUserToken(User user, String jwtToken, String refreshToken) {
+        var token = Token.builder()
+            .user(user)
+            .accessToken(jwtToken)
+            .refreshToken(refreshToken)
+            .tokenType(TokenType.BEARER)
+            .expired(false)
+            .revoked(false)
+            .build();
+        tokenRepository.save(token);
     }
 
     // Helper method to invalidate all prior user tokens to make room for a new one.
