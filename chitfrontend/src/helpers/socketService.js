480fe8a07dea76ea3a37c2a132a56e54aa3eddec
IsraelBanez <isbanez@calpoly.edu>
2024-02-12 01:38:54 -0800

    set up ping pong for client side

--- chitfrontend/src/helpers/socketService.js
+++ chitfrontend/src/helpers/socketService.js
@@ -7,31 +7,58 @@ const connectSocket = () => {
     const stompClient = Stomp.over(() => socket);
 
     stompClient.connect({}, () => {
-        console.log('Connected to WebSocket');
-        subscribeToHalfwayPoint(stompClient);
+        console.log("Connected to WebSocket.");
+
+        tokenHalfWayCheck(stompClient);
+
+        heatBeatCheck(stompClient);
+    },
+    (error) => {
+        console.error("Error connecting to WebSocket:", error);
     });
 };
 
-const disconnectSocket = () => {
+const disconnectSocket = (stompClient) => {
     if (stompClient) {
         stompClient.deactivate();
-        console.log('Disconnected from WebSocket');
+        console.log("Disconnected from WebSocket");
+    } else {
+        console.error("Error disconnecting from WebSocket.")
     }
 };
 
-const subscribeToHalfwayPoint = (stompClient) => { 
-    const topic = '/user/specific/token-halfway-check';
-    stompClient.subscribe(topic, function(message) {
+// Tell the client that the available toke is about to expire.
+const tokenHalfWayCheck = (stompClient) => { 
+    const destination = '/user/specific/token-halfway-check';
+    stompClient.subscribe(destination, function(message) {
         const parsedMessage = JSON.parse(message.body);
         const payload = parsedMessage.payload;
         const tokenRefreshStatus = parsedMessage.headers['tokenRefresh'];
 
-        console.log('Received halfway check notification:', payload);
-        console.log('Token refresh status:', tokenRefreshStatus);
+        console.log("Received token halfway check:", payload);
+        console.log("Token refresh status:", tokenRefreshStatus);
     },
     (error) => {
-        console.error('Error subscribing to WebSocket:', error);
+        console.error("Error subscribing to halfway check:", error);
     });
 };
 
-export { connectSocket, disconnectSocket, subscribeToHalfwayPoint };
+// Keep a heart beat pattern to indicate that the user is active
+const heatBeatCheck = (stompClient) => {
+    const destination = '/user/specific/ping';
+    stompClient.subscribe(destination, function(message) {
+        const parsedMessage = message.body;
+        console.log("Received heatbeat check:", parsedMessage);
+
+        if (parsedMessage === "ping"){
+            console.log("Sending pong back.");
+            stompClient.send('/app/pong', {}, "pong");
+        }
+    }, 
+    (error) => {
+        console.error("Error subscribing to heat beat check:", error);
+    });
+
+}
+
+export { connectSocket, disconnectSocket, tokenHalfWayCheck, heatBeatCheck };
